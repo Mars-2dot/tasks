1584af5277c6c0653e3188369193d59918741d52
auspehov <auspehov@inobitec.com>
2021-04-06 17:06:22 +0300

    fix cmake configure

--- 3rd/ExternalProjectBoost.cmake
+++ 3rd/ExternalProjectBoost.cmake
@@ -13,7 +13,7 @@ ExternalProject_Add( external_boost
             --with-libraries=program_options 
 	    --with-libraries=filesystem 
 	        BUILD_COMMAND
-	     b2 toolset=msvc --address-model=64 --link=static --variant=release stage install 
+	     b2 toolset=msvc address-model=64 link=static variant=release,debug stage install 
         INSTALL_COMMAND ""
 	)
 include_directories(${CMAKE_CURRENT_BINARY_DIR}/boost/src/external_boost) 
@@ -22,16 +22,16 @@ link_directories(${CMAKE_CURRENT_BINARY_DIR}/boost/src/external_boost/stage/lib)
 set( boost_INSTALL ${CMAKE_CURRENT_BINARY_DIR}/3rd_part )
 set( boost_LIB_DIRS ${CMAKE_CURRENT_BINARY_DIR}/boost/src/external_boost/stage/lib )
 set( BOOST_ROOT ${CMAKE_CURRENT_BINARY_DIR}/boost/src/external_boost )
-set( boost_INCLUDE_DIRS ${CMAKE_CURRENT_BINARY_DIR}/boost/src/external_boost)
+set( Boost_INCLUDE_DIR ${CMAKE_CURRENT_BINARY_DIR}/boost/src/external_boost)
 list (APPEND EXTRA_CMAKE_ARGS
   -DBoost_NO_SYSTEM_PATHS=ON)
 add_library( boost::program_options  STATIC IMPORTED )
 set_property( TARGET boost::program_options PROPERTY IMPORTED_LOCATION ${boost_LIB_DIRS}/libboost_program_options.lib )
-set_property( TARGET boost::program_options PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${boost_INCLUDE_DIRS} )
+set_property( TARGET boost::program_options PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIR} )
 add_dependencies( boost::program_options  external_boost )
 
 add_library( boost::filesystem   STATIC IMPORTED )
 set_property( TARGET boost::filesystem  PROPERTY IMPORTED_LOCATION ${boost_LIB_DIRS}/libboost_filesystem.lib )
-set_property( TARGET boost::filesystem PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${boost_INCLUDE_DIRS} )
+set_property( TARGET boost::filesystem PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${Boost_INCLUDE_DIR} )
 add_dependencies( boost::filesystem  external_boost )
 
